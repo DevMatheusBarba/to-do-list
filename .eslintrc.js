a32572a726ba4ad8540089b9d122ae6460b7897b
Matheus casa <matheus.silva081@icloud.com>
2025-08-22 16:21:03 -0300

    Ajuste de rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
     // Permite JSX em arquivos .js e .jsx
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/state-in-constructor': [0],
+    'react/forbid-prop-types': [0],
   },
 };
